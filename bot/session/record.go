cc8502ea754e4b65dd716e1f42d5436e4d2295ab
Dylan Madisetti <contact@dylanmadisetti.com>
2015-05-26 17:31:21 -0400

    Records were broken

--- bot/session/record.go
+++ bot/session/record.go
@@ -18,12 +18,11 @@ type Record struct {
 func (s *Session) GetRecords() (records *Record){
     records = &Record{}
     err := datastore.Get(s.context,datastore.NewKey(s.context,"Records","",1, nil),records)
-    if err !=nil{
-        s.context.Infof("Records: %v",Saved)
+    if err == nil{
+        s.context.Infof("Records: %v","Saved")
         s.SaveRecords(records)
         return
     }
-    s.context.Infof("Error: %v", err)
     return
 }
 
